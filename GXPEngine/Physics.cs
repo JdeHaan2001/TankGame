5b9d8cf1cd82902bd2fa8d5724d400139469e3b8
Jeroen de Haan <info.jdehaan@gmail.com>
2020-04-22 13:01:51 +0200

    Add project files.

---
+++ GXPEngine/Physics.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+namespace GXPEngine
+{
+    /// <summary>
+    /// Physics class.
+    /// Made by Jeroen de Haan 478343
+    /// DO NOT USE WITHOUT APPROVAL FROM JEROEN DE HAAN
+    /// </summary>
+    public static class Physics
+    {
+        /// <summary>
+        /// Calculates the point of impact (PoI) of an object when it collides with a border. This doesn't work between
+        /// collisions between 2 different objects.
+        /// </summary>
+        /// <param name="oldPos"></param>
+        /// <param name="velocity"></param>
+        /// <param name="currentPos"></param>
+        /// <param name="boundary"></param>
+        /// <param name="radius"></param>
+        /// <returns>This returns the point of impact</returns>
+        public static Vec2 PoIBorderHorizontal(Vec2 oldPos, Vec2 velocity, Vec2 currentPos, float boundary, float radius)
+        {
+            bool isGoingRight;
+            if (currentPos.x < boundary)
+                isGoingRight = true;
+            else
+                isGoingRight = false;
+            float radiusOffSet = isGoingRight ? radius : -radius;
+            float a = oldPos.x - (boundary - radiusOffSet);
+            float b = oldPos.x - currentPos.x;
+            float t = a / b;
+            Vec2 PoI = oldPos + t * velocity;
+            return PoI;
+        }
+
+        /// <summary>
+        /// Calculates the point of impact (PoI) of an object when it collides with a border. This doesn't work between
+        /// collisions between 2 different objects. 
+        /// </summary>
+        /// <param name="oldPos"></param>
+        /// <param name="velocity"></param>
+        /// <param name="currentPos"></param>
+        /// <param name="boundary"></param>
+        /// <param name="radius"></param>
+        /// <returns></returns>
+        public static Vec2 PoIBorderVertical(Vec2 oldPos, Vec2 velocity, Vec2 currentPos, float boundary, float radius)
+        {
+            bool isGoingDown;
+            if (currentPos.y < boundary)
+                isGoingDown = true;
+            else
+                isGoingDown = false;
+            float radiusOffSet = isGoingDown ? radius : -radius;
+            float a = oldPos.y - (boundary - radiusOffSet);
+            float b = oldPos.y - currentPos.y;
+            float t = a / b;
+            Vec2 PoI = oldPos + t * velocity;
+
+            return PoI;
+        }
+
+        /// <summary>
+        /// This returns the Time of Impact (TOI) for blocks colliding horizontally
+        /// </summary>
+        /// <param name="oldPos"></param>
+        /// <param name="velocity"></param>
+        /// <param name="currentPos"></param>
+        /// <param name="otherPos"></param>
+        /// <param name="otherVelocity"></param>
+        /// <param name="radius"></param>
+        /// <param name="otherRadius"></param>
+        /// <returns></returns>
+        public static float TimeOfImpactHorizontal(Vec2 oldPos, Vec2 currentPos,
+                                                   Vec2 otherPos, float radius, float otherRadius)
+        {
+            bool isGoingRight;
+            if (currentPos.x < otherPos.x)
+                isGoingRight = true;
+            else
+                isGoingRight = false;
+            float radiusOffset = isGoingRight ? radius : -radius;
+            float otherRadiusOffset = isGoingRight ? -otherRadius : otherRadius;
+            float a = oldPos.x - (otherPos.x + otherRadiusOffset + radiusOffset);
+            float b = oldPos.x - currentPos.x;
+            float t = a / b;
+            return t;
+        }
+
+        /// <summary>
+        /// This return the Time of Impact (TOI) for blocks colliding vertically
+        /// </summary>
+        /// <param name="oldPos"></param>
+        /// <param name="velocity"></param>
+        /// <param name="currentPos"></param>
+        /// <param name="otherPos"></param>
+        /// <param name="otherVelocity"></param>
+        /// <param name="radius"></param>
+        /// <param name="otherRadius"></param>
+        /// <returns></returns>
+        public static float TimeOfImpactVertical(Vec2 oldPos, Vec2 currentPos,
+                                                 Vec2 otherPos, float radius, float otherRadius)
+        {
+            bool isGoingDown;
+            if (currentPos.y < otherPos.y)
+                isGoingDown = true;
+            else
+                isGoingDown = false;
+            float radiusOffset = isGoingDown ? radius : -radius;
+            float otherRadiusOffset = isGoingDown ? -otherRadius : otherRadius;
+            float a = oldPos.y - (otherPos.y + otherRadiusOffset + radiusOffset);
+            float b = oldPos.y - currentPos.y;
+            float t = a / b;
+            return t;
+        }
+        /// <summary>
+        /// Calculates
+        /// </summary>
+        /// <param name="ballPos"></param>
+        /// <param name="lineStart"></param>
+        /// <param name="lineEnd"></param>
+        /// <returns></returns>
+        public static float CalculateBallDist(Vec2 ballPos, Vec2 lineStart, Vec2 lineEnd)
+        {
+            Vec2 a = ballPos - lineStart;
+            Vec2 b = lineEnd - lineStart;
+            Vec2 bNorm = b.Normalized();
+            float pLength = a.Dot(bNorm);
+            Vec2 pVec = pLength * bNorm;
+            Vec2 distVec = a - pVec;
+            return distVec.Length;
+
+        }
+    }
+}
---
+++ GXPEngine/Vec2.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using GXPEngine;
+
+public struct Vec2
+{
+	public float x;
+	public float y;
+
+	public Vec2(float pX = 0, float pY = 0)
+	{
+		x = pX;
+		y = pY;
+	}
+	/// <summary>
+	/// Sets the x & y of the current vector
+	/// </summary>
+	/// <param name="x"></param>
+	/// <param name="y"></param>
+	public void SetXY(float x, float y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+	public static Vec2 operator +(Vec2 left, Vec2 right)
+	{
+		return new Vec2(left.x + right.x, left.y + right.y);
+	}
+
+	public static Vec2 operator -(Vec2 left, Vec2 right)
+	{
+		return new Vec2(left.x - right.x, left.y - right.y);
+	}
+
+	public static Vec2 operator *(Vec2 left, float multiplier)
+	{
+		return new Vec2(left.x * multiplier, left.y * multiplier);
+	}
+
+	public static Vec2 operator *(float multiplier, Vec2 left)
+	{
+		return new Vec2(left.x * multiplier, left.y * multiplier);
+	}
+
+	public static Vec2 operator /(Vec2 v, float scalar)
+	{
+		return new Vec2(v.x / scalar, v.y / scalar);
+	}
+
+
+	public Vec2 Multiply(float amountX, float amountY)
+	{
+		x *= amountX;
+		y *= amountY;
+		return this;
+	}
+
+	/// <summary>
+	/// Gets the length of the current vector
+	/// </summary>
+	public float Length
+	{
+		get { return (float)Math.Pow((x * x) + (y * y), 0.5); }
+	}
+
+	/// <summary>
+	/// Returns a normalized version of the current vector without modifying it
+	/// </summary>
+	/// <returns></returns>
+	public Vec2 Normalized()
+	{
+		float length = Length;
+		if (length == 0)
+		{
+			return this;
+		}
+		return new Vec2(x / length, y / length);
+	}
+
+	/// <summary>
+	/// Normalizes the current vector
+	/// </summary>
+	/// <returns></returns>
+	public Vec2 Normalize()
+	{
+		if (Length == 0)
+		{
+			return this;
+		}
+		SetXY(x / Length, y / Length);
+		return this;
+	}
+
+	/// <summary>
+	/// Returns a string representation of the current vector
+	/// </summary>
+	/// <returns></returns>
+	public override string ToString()
+	{
+		return String.Format("({0},{1})", x, y);
+	}
+
+	/// <summary>
+	/// Converts given degrees to radians
+	/// </summary>
+	/// <param name="deg"></param>
+	/// <returns></returns>
+	public static float Deg2Rad(float deg)
+	{
+		return deg * (Mathf.PI / 180);
+	}
+
+	/// <summary>
+	/// Converts given radians to degrees
+	/// </summary>
+	/// <param name="rad"></param>
+	/// <returns></returns>
+	public static float Rad2Deg(float rad)
+	{
+		float degrees;
+		degrees = (float)(rad * 180 / Mathf.PI);
+		return degrees;
+	}
+
+	/// <summary>
+	/// Returns a new vector with given angle in radians
+	/// </summary>
+	/// <param name="rad"></param>
+	/// <param name="length"></param>
+	/// <returns></returns>
+	public static Vec2 GetUnitVecRad(float rad, float length = 1)
+	{
+		float nx = Mathf.Cos(rad) * length;
+		float ny = Mathf.Sin(rad) * length;
+		return new Vec2(nx, ny);
+	}
+
+	/// <summary>
+	/// Returns a new vector with given angle in degrees
+	/// </summary>
+	/// <param name="degrees"></param>
+	/// <param name="length"></param>
+	/// <returns></returns>
+	public static Vec2 GetUnitVecDeg(float degrees, float length = 1)
+	{
+		float nx = Mathf.Cos(Deg2Rad(degrees)) * length;
+		float ny = Mathf.Sin(Deg2Rad(degrees)) * length;
+		return new Vec2(nx, ny);
+	}
+
+	/// <summary>
+	/// returns a vector with a random angle
+	/// </summary>
+	/// <param name="length"></param>
+	/// <returns></returns>
+	public static Vec2 RandomUnitVector()
+	{
+		float nx = Mathf.Cos(Deg2Rad(Utils.Random(0, 360)));
+		float ny = Mathf.Sin(Deg2Rad(Utils.Random(0, 360)));
+		return new Vec2(nx, ny);
+	}
+
+	/// <summary>
+	/// Sets the angle of the vector to a given angle in radians
+	/// </summary>
+	/// <param name="rad"></param>
+	/// <returns></returns>
+	public Vec2 SetAngleRad(float rad)
+	{
+		this = Vec2.GetUnitVecRad(rad) * Length;
+
+		return this;
+	}
+
+	/// <summary>
+	/// Sets the angle of the vector to a given angle in degrees
+	/// </summary>
+	/// <param name="deg"></param>
+	/// <returns></returns>
+	public Vec2 SetAngleDegrees(float deg)
+	{
+		return SetAngleRad(Deg2Rad(deg));
+	}
+
+
+	/// <summary>
+	/// returns the angle of the vector in radians
+	/// </summary>
+	/// <returns></returns>
+	public float getAngleRad()
+	{
+		return Mathf.Atan2(y, x);
+	}
+
+	/// <summary>
+	/// returns the angle of the delta vector between 2 points
+	/// </summary>
+	/// <param name="a"></param>
+	/// <param name="b"></param>
+	/// <returns></returns>
+	public float GetAngleDeg2Points(Vec2 a, Vec2 b)
+	{
+		return Rad2Deg(Mathf.Atan2(b.y - a.y, b.x - a.x));
+	}
+	/// <summary>
+	/// Returns the angle of the vector in degrees
+	/// </summary>
+	/// <returns></returns>
+	public float getAngleDeg()
+	{
+		return Vec2.Rad2Deg(Mathf.Atan2(y, x));
+	}
+
+	/// <summary>
+	/// Rotates the vector by adding given radians to the current angle of the vector
+	/// </summary>
+	/// <param name="addedRotationRad"></param>
+	public void RotateRadians(float addedRotationRad)
+	{
+		float sinAngle = Mathf.Sin(addedRotationRad);
+		float cosAngle = Mathf.Cos(addedRotationRad);
+		this = new Vec2(x * cosAngle - y * sinAngle, x * sinAngle + y * cosAngle);
+	}
+
+	/// <summary>
+	/// Rotates the vector by adding given degrees to the current angle of the vector
+	/// </summary>
+	/// <param name="addedRotationDeg"></param>
+	public void RotateDegrees(float addedRotationDeg)
+	{
+		addedRotationDeg = Deg2Rad(addedRotationDeg);
+		RotateRadians(addedRotationDeg);
+	}
+
+	/// <summary>
+	/// Rotates an object around a target by adding the given angle in radians to the current angle of the delta vector between the objects
+	/// </summary>
+	/// <param name="targetPoint"></param>
+	/// <param name="rad"></param>
+	public void RotateAroundRad(Vec2 targetPoint, float rad)
+	{
+		this = this - targetPoint;
+		RotateRadians(rad);
+		this = this + targetPoint;
+	}
+
+	/// <summary>
+	/// Rotates an object around a target by adding the given angle in degrees to the current angle of the delta vector between the objects
+	/// </summary>
+	/// <param name="targetPoint"></param>
+	/// <param name="deg"></param>
+	public void RotateAroundDeg(Vec2 targetPoint, float deg)
+	{
+		this = this - targetPoint;
+		RotateDegrees(deg);
+		this = this + targetPoint;
+	}
+
+	/// <summary>
+	/// This calculates the dot product and returns the product in a float. This function does not normalize a vector.
+	/// </summary>
+	/// <param name="other"></param>
+	/// <returns></returns>
+	public float Dot(Vec2 other)
+	{
+		float dotProduct = this.x * other.x + this.y * other.y;
+		return dotProduct;
+	}
+
+	public Vec2 Normal()
+	{
+		Normalize();
+		return new Vec2(-this.y, this.x);
+	}
+
+	public void Reflect(Vec2 pNormal, float bounciness = 1f)
+	{
+		Vec2 normalNormal = pNormal.Normal();
+		Vec2 outVec = this - (1 + bounciness) * (this.Dot(normalNormal)) * normalNormal;
+		this = outVec;
+	}
+}
+
